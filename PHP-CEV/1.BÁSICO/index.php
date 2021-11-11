2bb735c38fb1dbc7d91daeb84cdaf7aec9867155
jonesramos01 <jonjones.rj@gmail.com>
2021-11-11 07:51:31 -0300

    commit

--- PHP-CEV/1.BÁSICO/index.php
+++ PHP-CEV/1.BÁSICO/index.php
@@ -4,14 +4,23 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Testanto PHP</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+
+    
 </head>
 <body>
     <h1>Testando PHP</h1>
-
+    <hr><br>
     <?php
 
-    echo "Hello World!"
+    echo "Hello World!<br>";
+
+    echo "<strong>Hello World!</strong><br>";
+
+    echo "<em>Hello World!</em><br>";
+
+    echo "<u>Hello World!</u><br>";
 
     ?>
 
