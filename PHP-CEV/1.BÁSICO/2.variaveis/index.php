83681908216bb85cce59d7333a199ee506d92ffe
jonesramos01 <jonjones.rj@gmail.com>
2021-11-12 09:06:01 -0300

    commit

---
+++ PHP-CEV/1.BÁSICO/2.variaveis/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>VARIÁVEIS</title>
+</head>
+<body>
+    <h1>VARIÁVEIS</h1>
+    <hr><br>
+    <?php
+        echo "Hello World!" //echo é similar ao escreva do algoritmo
+
+
+    ?>
+    
+</body>
+</html>
