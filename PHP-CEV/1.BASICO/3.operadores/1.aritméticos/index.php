57d13d8ca274d19fe6e85cdffee66a62e93d096b
jonesramos01 <jonjones.rj@gmail.com>
2021-11-14 08:47:58 -0300

    commit

---
+++ PHP-CEV/1.BASICO/3.operadores/1.aritméticos/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>OPERADORES ARITMÉTICOS</title>
+</head>
+<body>
+    <h1>OPERADORES ARITMÉTICOS</h1>
+    <hr>
+    <?php
+    
+    /**/
+
+    $n1 = 2;
+    $n2 = 3;
+    $s = $n1 + $n2;
+    echo "A soma vale $s";
+    
+    
+    ?>
+
+</body>
+</html>
